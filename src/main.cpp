b085d4d42eb2197493cd2f2cbba0911234c4a2cf
Stepanium <stepnium@gmail.com>
2023-08-31 14:38:13 +0700

    GAMEPLAY LOOP ADDED, WOOOOOOH!!!

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,9 @@ enum
     CELL_PATH_EAST = 0x02,
     CELL_PATH_SOUTH = 0x04,
     CELL_PATH_WEST = 0x08,
-    CELL_VISITED = 0x10
+    CELL_VISITED = 0x10,
+    CELL_START = 0x20,
+    CELL_FINISH = 0x40
 };
 
 struct maze
@@ -25,11 +27,21 @@ struct maze
     olc::Pixel floorColor;
     olc::Pixel wallColor;
 
+    olc::Pixel startColor;
+    olc::Pixel finishColor;
+    int start_x, start_y;
+    int finish_x, finish_y;
+
     void GenerateMaze(int m_nMazeWidth, int m_nMazeHeight)
     {
-
         this->m_nMazeWidth = m_nMazeWidth;
         this->m_nMazeHeight = m_nMazeWidth;
+
+        start_x = (int)((float)m_nMazeWidth * 0.5f);
+        start_y = m_nMazeHeight - 1;
+        finish_x = start_x;
+        finish_y = 0;
+
         m_nMaze = new int[m_nMazeWidth * m_nMazeHeight];
 
         memset(m_nMaze, 0x00, m_nMazeWidth * m_nMazeHeight * sizeof(int));
@@ -49,6 +61,10 @@ struct maze
 
         while (m_nVisitedCells < m_nMazeHeight * m_nMazeWidth)
         {
+            if (m_stack.top().first == start_x && m_stack.top().second == start_y)
+                m_nMaze[offset(0, 0)] |= CELL_START;
+            else if (m_stack.top().first == finish_x && m_stack.top().second == finish_y)
+                m_nMaze[offset(0, 0)] |= CELL_FINISH;
 
             // Create a set of the unvisited neighbours
             vector<int> neighbours;
@@ -205,7 +221,7 @@ struct player
     float speed = 16.0f;
 
     olc::Pixel color;
-    olc::Pixel antiColor;
+    // olc::Pixel antiColor;
 
     void Move(vec2d direction, maze maze, int m_nTileWidth, int m_nPathWidth, int m_nWallWidth, float fElapsedTime)
     {
@@ -307,9 +323,47 @@ private:
     int m_nPathWidth;
     int m_nTileWidth;
     int m_nWallWidth;
-    player p;
+    player p_player;
 
-    bool light = true;
+    bool bLight = true;
+    bool bExplore = true; //is gameplay right now in the explore mode or int the remember and find the exit mode?
+
+
+    void DrawMaze(maze m_maze, player p_player, bool bLight)
+    {
+        for (int x = 0; x < m_maze.m_nMazeWidth; x++)
+        {
+            for (int y = 0; y < m_maze.m_nMazeHeight; y++)
+            {
+                olc::Pixel color = (m_maze.m_nMaze[y * m_maze.m_nMazeWidth + x] & CELL_START) ? m_maze.startColor : (m_maze.m_nMaze[y * m_maze.m_nMazeWidth + x] & CELL_FINISH) ? m_maze.finishColor :  m_maze.floorColor;
+                int x_transformed = m_nWallWidth + x * m_nTileWidth;
+                int y_transformed = m_nWallWidth + y * m_nTileWidth;
+                vec2d center = {(float)x_transformed + 0.5f * m_nTileWidth, (float)y_transformed + 0.5f * m_nTileWidth};
+                if (bLight || x_transformed > ScreenWidth() || y_transformed > ScreenHeight() || (p_player.pos - center).GetLengthSqared() < p_player.visionRadius * p_player.visionRadius)
+                {
+                    FillRect(x_transformed, y_transformed, m_nPathWidth, m_nPathWidth, color);
+
+                    // Draw passageways between cells
+                    for (int p = 0; p < m_nPathWidth; p++)
+                    {
+                        for (int k = 0; k < m_nWallWidth; k++)
+                        {
+                            if (m_maze.m_nMaze[y * m_maze.m_nMazeWidth + x] & CELL_PATH_SOUTH)
+                                Draw(x_transformed + p, y_transformed + m_nPathWidth + k, color); // Draw South Passage
+
+                            if (m_maze.m_nMaze[y * m_maze.m_nMazeWidth + x] & CELL_PATH_EAST)
+                                Draw(x_transformed + m_nPathWidth + k, y_transformed + p, color); // Draw East Passage
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void DrawPlayer(player p)
+    {
+        FillCircle((int)p.pos.x, (int)p.pos.y, (int)p.radius, p.color);
+    }
 
 public:
     MMM()
@@ -320,28 +374,31 @@ public:
 protected:
     bool OnUserCreate() override
     {
-        m_maze.GenerateMaze(16, 16);
-        m_nPathWidth = 32;
+        m_maze.GenerateMaze(17, 17);
+        m_nPathWidth = 30;
         m_nWallWidth = 2;
         m_nTileWidth = m_nPathWidth + m_nWallWidth;
         m_maze.wallColor = olc::Pixel(10, 10, 10);
         m_maze.floorColor = olc::Pixel(240, 160, 0);
+        m_maze.wallColor = olc::Pixel(10, 10, 10);
 
-        p.pos = {250.0f, 535.0f};
-        p.radius = 2.0f;
-        p.speed = 128.0f;
-        p.visionRadius = 2.0f * m_nTileWidth;
-        p.color = m_maze.wallColor;
-        p.antiColor = olc::Pixel(255 - p.color.r, 255 - p.color.g, 255 - p.color.b);
+        m_maze.startColor = olc::GREEN;
+        m_maze.finishColor = olc::RED;
+
+        p_player.pos = { (float)m_maze.m_nMazeWidth * m_nTileWidth * 0.5f, ((float)m_maze.m_nMazeWidth - 0.5f) * m_nTileWidth };
+        p_player.radius = 2.0f;
+        p_player.speed = 128.0f;
+        p_player.visionRadius = 2.0f * m_nTileWidth;
+        p_player.color = m_maze.wallColor;
+        // p_player.antiColor = olc::Pixel(255 - p_player.color.r, 255 - p_player.color.g, 255 - p_player.color.b);
 
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
-
-        // INPUT//
-        // TRANSLATION//
+        ///// INPUT/////
+        //translation input
         vec2d dir = {0, 0};
         if (GetKey(olc::Key::W).bHeld)
             dir.y -= 1.0f;
@@ -351,43 +408,43 @@ protected:
             dir.x -= 1.0f;
         if (GetKey(olc::Key::D).bHeld)
             dir.x += 1.0f;
-        //////
+        //ready to start
 
-        p.Move(dir, m_maze, m_nTileWidth, m_nPathWidth, m_nWallWidth, fElapsedTime);
+        if (GetKey(olc::Key::R).bPressed)
+            bExplore = false;
+        ///////////////
 
-        // DRAWING//
-        Clear(m_maze.wallColor);
 
-        // DRAW MAZE
-        for (int x = 0; x < m_maze.m_nMazeWidth; x++)
+        //// CALCULATION ////
+        if (!bExplore)
         {
-            for (int y = 0; y < m_maze.m_nMazeHeight; y++)
+            if (bLight)
             {
-                int x_transformed = m_nWallWidth + x * m_nTileWidth;
-                int y_transformed = m_nWallWidth + y * m_nTileWidth;
-                vec2d center = {(float)x_transformed + 0.5f * m_nTileWidth, (float)y_transformed + 0.5f * m_nTileWidth};
-                if (light || (p.pos - center).GetLengthSqared() < p.visionRadius * p.visionRadius)
-                {
-                    FillRect(x_transformed, y_transformed, m_nPathWidth, m_nPathWidth, m_maze.floorColor);
-
-                    // Draw passageways between cells
-                    for (int p = 0; p < m_nPathWidth; p++)
-                    {
-                        for (int k = 0; k < m_nWallWidth; k++)
-                        {
-                            if (m_maze.m_nMaze[y * m_maze.m_nMazeWidth + x] & CELL_PATH_SOUTH)
-                                Draw(x_transformed + p, y_transformed + m_nPathWidth + k, m_maze.floorColor); // Draw South Passage
-
-                            if (m_maze.m_nMaze[y * m_maze.m_nMazeWidth + x] & CELL_PATH_EAST)
-                                Draw(x_transformed + m_nPathWidth + k, y_transformed + p, m_maze.floorColor); // Draw East Passage
-                        }
-                    }
-                }
+                bLight = false;
+                p_player.pos = { ((float)m_maze.start_x + 0.5f) * m_nTileWidth, ((float)m_maze.start_y + 0.5f) * m_nTileWidth };
             }
         }
+        //player movement and collision resolution
+        p_player.Move(dir, m_maze, m_nTileWidth, m_nPathWidth, m_nWallWidth, fElapsedTime);
+        if ((int)(p_player.pos.x / (float)m_nTileWidth) == m_maze.finish_x && 
+            (int)(p_player.pos.y / (float)m_nTileWidth) == m_maze.finish_y)
+        {
+            bLight = true;
+            bExplore = true;
+            m_maze.GenerateMaze(17, 17);
+            p_player.pos = { ((float)m_maze.start_x + 0.5f) * m_nTileWidth, ((float)m_maze.start_y + 0.5f) * m_nTileWidth };
+        }
+        ///////////////
 
-        // DRAW PLAYER
-        FillCircle((int)p.pos.x, (int)p.pos.y, (int)p.radius, p.color);
+
+        ////DRAWING////
+        Clear(m_maze.wallColor);
+
+        //draw maze
+        DrawMaze(m_maze, p_player, bLight);
+        //draw player
+        DrawPlayer(p_player);
+        ////////////////
 
         return true;
     }
@@ -396,7 +453,7 @@ protected:
 int main()
 {
     MMM demo;
-    if (demo.Construct(600, 600, 1, 1))
+    if (demo.Construct(640, 640, 1, 1))
         demo.Start();
 
     return 0;
